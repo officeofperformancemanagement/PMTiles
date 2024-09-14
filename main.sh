a85003f6d3973fa33eb7601d3179f248dd50157d
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-09-13 20:45:01 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
-curl -L https://github.com/protomaps/go-pmtiles/releases/download/v1.21.0/go-pmtiles_1.21.0_Linux_x86_64.tar.gz | tar zx
+# download and extract only pmtiles file
+curl -L https://github.com/protomaps/go-pmtiles/releases/download/v1.21.0/go-pmtiles_1.21.0_Linux_x86_64.tar.gz | tar zx pmtiles
 
 ls -alsh .
 
