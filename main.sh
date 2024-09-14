4cd94a0d31338c11f855876054533cf2e87caea0
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-09-13 20:27:06 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -e
 
-curl "https://github.com/protomaps/go-pmtiles/releases/download/v1.21.0/go-pmtiles_1.21.0_Linux_x86_64.tar.gz" --output "go-pmtiles_1.21.0_Linux_x86_64.tar.gz"
+curl --output "go-pmtiles_1.21.0_Linux_x86_64.tar.gz" "https://github.com/protomaps/go-pmtiles/releases/download/v1.21.0/go-pmtiles_1.21.0_Linux_x86_64.tar.gz"
+
+ls -alsh .
 
 tar -xvzf ./go-pmtiles_1.21.0_Linux_x86_64.tar.gz
 
-ls .
+ls -alsh .
 
 # pmtiles extract https://build.protomaps.com/20240618.pmtiles chattanooga.pmtiles --bbox=-85.5754812346649,34.990282262603216,-85.00603778143356,35.30248201906815
