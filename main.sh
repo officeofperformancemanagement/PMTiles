db30e0e4cd4ddda53d7eb5fb30926283e3378481
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-09-13 20:40:18 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,3 +7,7 @@ ls -alsh .
 YESTERDAY=$(date '+%Y%m%d' -d "yesterday")
 
 ./pmtiles extract https://build.protomaps.com/$YESTERDAY.pmtiles chattanooga.pmtiles --bbox=-85.5754812346649,34.990282262603216,-85.00603778143356,35.30248201906815
+
+# cleanup
+rm go-pmtiles_1.21.0_Linux_x86_64.tar.gz
+rm pmtiles
