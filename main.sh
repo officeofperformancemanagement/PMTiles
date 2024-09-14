c00d9c65eb1fe29022d424dee63fa6260d4e36c9
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-09-13 20:38:03 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,4 +4,6 @@ curl -L https://github.com/protomaps/go-pmtiles/releases/download/v1.21.0/go-pmt
 
 ls -alsh .
 
-./pmtiles extract https://build.protomaps.com/20240618.pmtiles chattanooga.pmtiles --bbox=-85.5754812346649,34.990282262603216,-85.00603778143356,35.30248201906815
+YESTERDAY=$(date '+%Y%m%d' -d "yesterday")
+
+./pmtiles extract https://build.protomaps.com/$YESTERDAY.pmtiles chattanooga.pmtiles --bbox=-85.5754812346649,34.990282262603216,-85.00603778143356,35.30248201906815
